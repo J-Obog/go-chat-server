4de7a9202934d478300b711206ff8373ac880a2e
Joshua Obogbaimhe <joshua.obogbaimhe@segment.com>
2022-05-31 16:23:59 -0400

    adding server struct for handling api requests

--- api/server.go
+++ api/server.go
@@ -1,1 +1,16 @@
 package api
+
+import "github.com/gorilla/mux"
+
+type Server struct {
+	Host    string
+	Port    uint16
+	handler mux.Router
+}
+
+func NewServer(host string, port uint16) *Server {
+	server := &Server{Host: host, Port: port}
+	router := mux.NewRouter()
+	server.handler = *router
+	return server
+}
