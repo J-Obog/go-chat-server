38c85ed0308930414d41616db6fb169029dcf927
Joshua Obogbaimhe <joshua.obogbaimhe@segment.com>
2022-05-31 15:39:25 -0400

    adding message model to message service

--- api/service/message/model.go
+++ api/service/message/model.go
@@ -1,1 +1,13 @@
 package message
+
+import (
+	"time"
+
+	"github.com/segmentio/ksuid"
+)
+
+type Message struct {
+	ID      ksuid.KSUID `json:"id"`
+	Content string      `json:"content"`
+	SentAt  time.Time   `json:"sent_at"`
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/J-Obog/go-chat-server
+
+go 1.18
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/segmentio/ksuid v1.0.4 // indirect
+)
