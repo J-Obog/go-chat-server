8818b2bc288d7250e7a4cae516783c0269903802
Joshua Obogbaimhe <joshua.obogbaimhe@segment.com>
2022-05-31 16:40:18 -0400

    adding route handler for message store

--- api/service/message/router.go
+++ api/service/message/router.go
@@ -1,1 +1,24 @@
 package message
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func NewMessageRouter() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/message", getAllMessages).Methods("GET")
+	router.HandleFunc("/message", createNewMessage).Methods("POST")
+	return router
+}
+
+//get all messages within a specified time frame
+func getAllMessages(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//create a new message
+func createNewMessage(w http.ResponseWriter, r *http.Request) {
+
+}
