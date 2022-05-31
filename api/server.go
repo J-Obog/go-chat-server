8ffcfd7434aabfda9fbe8ff4ae1acb026cb130a5
Joshua Obogbaimhe <joshua.obogbaimhe@segment.com>
2022-05-31 16:33:48 -0400

    adding handler initializer function

--- api/server.go
+++ api/server.go
@@ -1,16 +1,42 @@
 package api
 
-import "github.com/gorilla/mux"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/gorilla/mux"
+)
+
+//a server to handle incoming api requests
+//handler is initialized with routes from service routers
 type Server struct {
-	Host    string
+	//host name
+	Host string
+
+	//port number
 	Port    uint16
-	handler mux.Router
+	handler *mux.Router
 }
 
 func NewServer(host string, port uint16) *Server {
-	server := &Server{Host: host, Port: port}
+	return &Server{
+		Host:    host,
+		Port:    port,
+		handler: initializedHandler(),
+	}
+}
+
+//adds service handlers to base routers and returns the newly intialized router
+func initializedHandler() *mux.Router {
 	router := mux.NewRouter()
-	server.handler = *router
-	return server
+	return router
+}
+
+//run server
+func (this *Server) Run() {
+	addr := fmt.Sprintf("%s:%d", this.Host, this.Port)
+	log.Fatal(http.ListenAndServe(addr, this.handler))
+	os.Exit(0)
 }
